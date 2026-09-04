0d66cf84040459ed70d5437a72d418177ac3efcf
Christopher <christophermikkelsen@live.dk>
2026-09-04 09:56:11 +0200

    Added and Implemented IGear

--- src/TheGoldenDice.Domain/Gear/Gear.cs
+++ src/TheGoldenDice.Domain/Gear/Gear.cs
@@ -1,6 +1,9 @@
+using TheGoldenDice.Domain.Items;
+
 namespace TheGoldenDice.Domain.Gear;
 
 internal sealed class Gear : IGear
 {
-    
+    public IHeadGear HeadSlot { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public IWeapon WeaponSlot { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 }
--- src/TheGoldenDice.Domain/Gear/IGear.cs
+++ src/TheGoldenDice.Domain/Gear/IGear.cs
@@ -1,6 +1,9 @@
+using TheGoldenDice.Domain.Items;
+
 namespace TheGoldenDice.Domain.Gear;
 
 public interface IGear
 {
-    
+    IHeadGear HeadSlot { get; set; }
+    IWeapon WeaponSlot { get; set; }
 }
