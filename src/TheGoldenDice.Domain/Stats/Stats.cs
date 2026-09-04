8b0cf90650dc24ebcc37cc0db9529ba8f339f8ef
Christopher <christophermikkelsen@live.dk>
2026-09-04 09:50:17 +0200

    Stats implements interface

---
+++ src/TheGoldenDice.Domain/Stats/Stats.cs
@@ -0,0 +1,9 @@
+namespace TheGoldenDice.Domain.Stats;
+
+internal sealed class Stats : IStats
+{
+    public int HPModifier { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public int AttackPower { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public int DefensePower { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public int Speed { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+}
