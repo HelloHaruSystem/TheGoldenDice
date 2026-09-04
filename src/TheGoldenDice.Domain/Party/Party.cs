bcd606d63efa7218a49028f338e8e2a1ebbd3522
Christopher <christophermikkelsen@live.dk>
2026-09-04 09:50:33 +0200

    Party implements interface

--- src/TheGoldenDice.Domain/Party/Party.cs
+++ src/TheGoldenDice.Domain/Party/Party.cs
@@ -1,6 +1,8 @@
+using TheGoldenDice.Domain.CharacterClasses;
+
 namespace TheGoldenDice.Domain.Party;
 
 internal sealed class Party : IParty
 {
-    
+    public List<BaseCharacter> Characters { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
 }
