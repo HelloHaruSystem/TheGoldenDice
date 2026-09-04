4e4e2772df9d08ac46b535e89a1e9f8a55588fb5
Christopher <christophermikkelsen@live.dk>
2026-09-04 10:00:49 +0200

    Added and Implemented IClass Updated documentation

--- src/TheGoldenDice.Domain/Classes/IClass.cs
+++ src/TheGoldenDice.Domain/Classes/IClass.cs
@@ -1,6 +1,13 @@
+using System.Net.Security;
+using TheGoldenDice.Domain.Stats;
+
 namespace TheGoldenDice.Domain.Classes;
 
-public interface IClass
+public interface IClass : IEquatable<IClass>
 {
-    
+    string Name { get; set; }
+    string Description { get; set; }
+
+    IStats GetStatsForLevel(int level);
+
 }
--- src/TheGoldenDice.Domain/Classes/TecTeacher.cs
+++ src/TheGoldenDice.Domain/Classes/TecTeacher.cs
@@ -1,6 +1,24 @@
+using TheGoldenDice.Domain.Stats;
+
 namespace TheGoldenDice.Domain.Classes;
 
 internal sealed class TecTeacher : IClass
 {
-    
+    public string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+    public string Description { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+    public bool Equals(IClass? other)
+    {
+        throw new NotImplementedException();
+    }
+
+    public override int GetHashCode()
+    {
+        return base.GetHashCode();
+    }
+
+    public IStats GetStatsForLevel(int level)
+    {
+        throw new NotImplementedException();
+    }
 }
