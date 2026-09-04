13702f6486d79efc8ff4819341d3b28fc1405de6
Christopher <christophermikkelsen@live.dk>
2026-09-04 10:03:19 +0200

    Update Documentation Update Namespace

--- src/TheGoldenDice.Domain/Character/BaseCharacter.cs
+++ src/TheGoldenDice.Domain/Character/BaseCharacter.cs
@@ -1,5 +1,5 @@
 using TheGoldenDice.Domain.Action;
-using TheGoldenDice.Domain.Characters;
+using TheGoldenDice.Domain.Classes;
 using TheGoldenDice.Domain.Gear;
 using TheGoldenDice.Domain.Items;
 using TheGoldenDice.Domain.Stats;
