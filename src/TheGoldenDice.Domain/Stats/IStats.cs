64fd4ee8fb27e70e13ac039d54d16221600e5ce6
Christopher <christophermikkelsen@live.dk>
2026-09-04 09:47:50 +0200

    Added IStats

--- src/TheGoldenDice.Domain/Stats/IStats.cs
+++ src/TheGoldenDice.Domain/Stats/IStats.cs
@@ -2,5 +2,8 @@ namespace TheGoldenDice.Domain.Stats;
 
 public interface IStats 
 {
-    
+    int HPModifier { get; set; }
+    int AttackPower { get; set; }
+    int DefensePower { get; set; }
+    int Speed { get; set; }
 }
