884dbb1337f5736b79950f302413007e8290eac2
Halfdan Sand Christensen <halfdansc@proton.me>
2026-09-07 12:31:42 +0200

    tech teacher getStatsForLevel implementation

--- src/TheGoldenDice.Domain/Classes/TecTeacher.cs
+++ src/TheGoldenDice.Domain/Classes/TecTeacher.cs
@@ -4,6 +4,14 @@ namespace TheGoldenDice.Domain.Classes;
 
 public sealed class TecTeacher : IClass
 {
+    private static readonly IStats _baseStats = new Stats.Stats
+    {
+        HPModifier = 10,
+        AttackPower = 3,
+        DefensePower = 2,
+        Speed = 5
+    };
+
     public string Name { get; set; } = "Tec Teacher";
     public string Description { get; set; } = "A teacher who would rather be outside having a smoke than teaching this class.";
 
@@ -18,6 +26,12 @@ public sealed class TecTeacher : IClass
 
     public IStats GetStatsForLevel(int level)
     {
-        throw new NotImplementedException();
+        return new Stats.Stats
+        {
+            HPModifier = _baseStats.HPModifier * level,
+            AttackPower = _baseStats.AttackPower * level,
+            DefensePower = _baseStats.DefensePower * level,
+            Speed = _baseStats.Speed * level
+        };
     }
 }
